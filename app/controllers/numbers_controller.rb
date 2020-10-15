dd6bc6aae82fdf93f52834cc1627d0c7ffc14c05
hgbooth <hgregor0@chicagobooth.edu>
2020-10-15 19:15:23 +0000

    Unlucky

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+
+  def losers
+    @res = Array.new
+
+    5.times do
+      cur = rand(1...100)
+      @res.push(cur)
+    end
+
+    render({ :template => "lottery_stuff/unlucky.html.erb"})
+  end
 end
